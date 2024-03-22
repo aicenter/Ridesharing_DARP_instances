197cb2c384932e31acfce624dbbe9d6c51c38846
Andres Sosa <aososam@gmail.com>
2024-03-22 10:46:04 +0100

    change: load request times as datetimes also

--- python/darpinstances/instance.py
+++ python/darpinstances/instance.py
@@ -1,5 +1,5 @@
 # from __future__ import annotations
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import math
 import os
@@ -284,21 +284,22 @@ def read_instance(filepath: Path, travel_time_provider: MatrixTravelTimeProvider
         while (line_string):
             line = line_string.split()
             request_id: int = int(index)
-            request_timestamp: int = int(line[0]) / 1000
-            request_datetime = datetime.fromtimestamp(request_timestamp)
+            if isinstance(line[0], int):
+                request_time = datetime.fromtimestamp(int(line[0]) / 1000)
+            else:
+                request_time = _load_datetime(line[0])
+
             start_node = Node(int(line[1]))
             end_node = Node(int(line[2]))
             equipment = map_equipment_type(line[4]).value if(len(line) > 4) else 0
             min_travel_time = travel_time_provider.get_travel_time(start_node, end_node)
-            max_pickup_timestamp = request_timestamp + int(instance_config['max_prolongation'])
-            max_pickup_datetime = datetime.fromtimestamp(max_pickup_timestamp)
-            min_drop_off_timestamp = request_timestamp + min_travel_time
-            max_drop_off_timestamp = max_pickup_timestamp + min_travel_time
-            min_drop_off_datetime = datetime.fromtimestamp(min_drop_off_timestamp)
-            max_drop_off_datetime = datetime.fromtimestamp(max_drop_off_timestamp)
+            max_pickup_time = request_time + timedelta(seconds=int(instance_config['max_prolongation']))
+            min_drop_off_time = request_time + timedelta(seconds=min_travel_time)
+            max_drop_off_time = max_pickup_time + timedelta(seconds=min_travel_time)
+
             vehicle_id = int(line[5]) if(len(line) > 5) else 0
-            requests.append(Request(request_id, action_id, start_node, request_datetime, max_pickup_datetime,
-                                    action_id + 1, end_node, min_drop_off_datetime,  max_drop_off_datetime,
+            requests.append(Request(request_id, action_id, start_node, request_time, max_pickup_time,
+                                    action_id + 1, end_node, min_drop_off_time,  max_drop_off_time,
                                     min_travel_time, 0, 0, equipment, vehicle_id))
             line_string = infile.readline()
             action_id += 2
