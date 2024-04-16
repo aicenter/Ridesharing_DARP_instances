9956c206e4790386aa92d94302765beed6b15249
Andres Sosa <aososam@gmail.com>
2024-04-16 11:56:44 +0200

    Update instance.py

--- python/darpinstances/instance.py
+++ python/darpinstances/instance.py
@@ -199,7 +199,6 @@ def map_equipment_type(equipment_str: str) -> EquipmentType:
         "STANDARD_SEAT": EquipmentType.STANDARD_SEAT,
         "WHEELCHAIR": EquipmentType.WHEELCHAIR,
         "ELECTRIC_WHEELCHAIR": EquipmentType.ELECTRIC_WHEELCHAIR,
-        "MEDICAL_STROLLER": EquipmentType.MEDICAL_STROLLER
         "MEDICAL_STROLLER": EquipmentType.MEDICAL_STROLLER,
         "SPECIAL_NEEDS_STROLLER": EquipmentType.SPECIAL_NEEDS_STROLLER,
     }
