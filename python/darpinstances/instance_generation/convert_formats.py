b5cc9386178f5661f66ca04977f821568c78f285
Domi Sidlova <domisidlova@gmail.com>
2024-11-05 13:11:43 +0100

    fix pipeline for trip sampling

--- python/darpinstances/instance_generation/convert_formats.py
+++ python/darpinstances/instance_generation/convert_formats.py
@@ -8,13 +8,16 @@ from pandas import DataFrame
 import geopandas as gpd
 from geopandas import GeoDataFrame
 from shapely.geometry import Point, LineString
+from datetime import datetime, timedelta
+
+from darpinstances.instance_generation.demand_generation import generate_uniform_trip_times
 
 PATH = Path(__file__).parent.parent.parent.parent
-
 RESOURCE_PATH = PATH / "resources/"
+BASETIME = datetime(2014, 1, 1)
 
 def import_nodes_tntp(file: str) -> DataFrame:
-    """Load nodes from a .tntp file convert coordinates to Point geometry."""
+    """Loads nodes from a TNTP file and converts coordinates to Point geometry."""
     nodes_df = pd.read_csv(file, delimiter='\t')
     nodes_df['geometry'] = nodes_df.apply(lambda row: Point(row['y'], row['x']), axis=1)
     nodes_df.set_index('node', inplace=True)
@@ -22,7 +25,7 @@ def import_nodes_tntp(file: str) -> DataFrame:
     return nodes_df
 
 def import_trips_tntp(file: str) -> DataFrame:
-    """Load trips from a .tntp file and keep non-null OD flows."""
+    """Loads trips from a TNTP file and keeps non-null OD flows."""
     with open(file, 'r') as f:
         all_rows = f.read()
 
@@ -42,19 +45,8 @@ def import_trips_tntp(file: str) -> DataFrame:
     matrix_df = pd.DataFrame(data)
     return matrix_df
 
-# def generate_trips(od_flow: float, origin: Point, destination: Point, time_period: int = 100):
-#     """Generate trips using a uniform distribution based on OD flows.
-#     Time period is 100 hours by default."""
-#     total_trips = int(od_flow * time_period)
-    
-#     timestamps = np.random.uniform(0, time_period, total_trips)
-    
-#     trips = [(origin, destination, timestamp) for timestamp in timestamps]
-    
-#     return trips
-
-def add_geometry(matrix_df: DataFrame, nodes_df: DataFrame) -> DataFrame:
-    """Add geometry from nodes_df to matrix_df."""
+def add_geometry(matrix_df: DataFrame, nodes_df: DataFrame, hours: int = 24) -> DataFrame:
+    """Adds geometry to matrix_df and generates trips based on OD flow."""
     data = []
     
     for _, row in matrix_df.iterrows():
@@ -65,51 +57,49 @@ def add_geometry(matrix_df: DataFrame, nodes_df: DataFrame) -> DataFrame:
         orig_coords = nodes_df.loc[origin, 'geometry']
         dest_coords = nodes_df.loc[destination, 'geometry']
         
-        # trips = generate_trips(flow, orig_coords, dest_coords)
+        trip_count = generate_trip_counts(flow, hours)
+        seconds = hours * 60 * 60
+        trip_times = generate_uniform_trip_times(0, seconds, trip_count)
 
-        # for trip in trips:
-        #     data.append({
-        #         'origin': orig_coords,
-        #         'destination': dest_coords,
-        #         'timestamp': trip[2],
-        #     })
-        data.append({
-            'origin': orig_coords,
-            'destination': dest_coords,
-            'od_flow': flow
-        })
+        for ttime in trip_times:
+            timestamp = BASETIME + timedelta(milliseconds=ttime)
+            data.append({
+                'origin': orig_coords,
+                'destination': dest_coords,
+                'timestamp': timestamp
+            })
 
     return pd.DataFrame(data)
 
 def load_matrix_from_csv(file: str) -> DataFrame:
-    """Load the matrix with coordinates from a CSV file."""
+    """Loads the matrix with coordinates from a CSV file."""
     return pd.read_csv(file)
 
-def convert_TNTP_format():
+def convert_TNTP_format(city: str):
+    tripstntpfile = RESOURCE_PATH / f"{city}_trips.tntp"
+    nodesfile = RESOURCE_PATH / f"{city}_node.tntp"
+    tripsfile = RESOURCE_PATH / f"{city}_trips.csv"
+
     # process trips to contain only valid OD flows
-    tripstntpfile = RESOURCE_PATH / "Sydney_trips.tntp"
-    matrix_df = import_trips_tntp(tripstntpfile)
-    flowscsvfile = RESOURCE_PATH / "Sydney_flows_no_geom.csv"
-    matrix_df.to_csv(flowscsvfile, index=False)
+    flowscsvfile = RESOURCE_PATH / f"{city}_flows_no_geom.csv"
+    matrix_df = load_matrix_from_csv(flowscsvfile)
+    # matrix_df = import_trips_tntp(tripstntpfile)
 
     # convert coordinates to proper geometry
-    nodesfile = RESOURCE_PATH / "Sydney_node.tntp"
     nodes_df = import_nodes_tntp(nodesfile)
 
-    # add geometry coordinates to trips
-    # matrix_df = load_matrix_from_csv(flowscsvfile)
+    # add geometry coordinates to flows and generate trips
     trips = add_geometry(matrix_df, nodes_df)
 
-    # save trips with geometry
-    tripsfile = RESOURCE_PATH / "Sydney_trips.csv"
+    # save trips to CSV
     trips.to_csv(tripsfile, index=False)
 
-def convert_CSV_format(filepath: str) -> tuple[GeoDataFrame, GeoDataFrame]:
-    with open(filepath) as csvfile:
+def convert_CSV_format(city: str) -> GeoDataFrame:
+    input_file = RESOURCE_PATH / f"{city}_trips.csv"
+    with open(input_file) as csvfile:
         data = list(csv.DictReader(csvfile))
 
     travel_requests = []
-    vehicles = []
 
     for request in data:
         # convert from str to list of coords
@@ -134,32 +124,21 @@ def convert_CSV_format(filepath: str) -> tuple[GeoDataFrame, GeoDataFrame]:
         }
         travel_requests.append(trip_req)
 
-        vehicle = {
-            "taxi_id": request["TAXI_ID"],
-            "starting_position": Point(polyline[0])
-        }
-        vehicles.append(vehicle)
-
     travel_requests_gdf = gpd.GeoDataFrame(travel_requests, crs="EPSG:4326")
     travel_requests_gdf.set_geometry("geometry", inplace=True)
 
-    vehicles_df = gpd.GeoDataFrame(vehicles, crs="EPSG:4326", geometry="starting_position")
-    vehicles_df.set_geometry("starting_position", inplace=True)
-    return travel_requests_gdf, vehicles_df
+    return travel_requests_gdf
 
-def generate_trips():
-     # process trips to contain only valid OD flows
-    # tripstntpfile = RESOURCE_PATH / "Sydney_trips.tntp"
-    # matrix_df = import_trips_tntp(tripstntpfile)
-    flowscsvfile = RESOURCE_PATH / "Sydney_flows_no_geom.csv"
-    # matrix_df.to_csv(flowscsvfile, index=False)
+def generate_trips(city: str):
+    flowscsvfile = RESOURCE_PATH / f"{city}_flows_no_geom.csv"
+    nodesfile = RESOURCE_PATH / f"{city}_node.tntp"
+    tripsfile = RESOURCE_PATH / f"{city}_trips.csv"
 
     # # convert coordinates to proper geometry
     nodesfile = RESOURCE_PATH / "Sydney_node.tntp"
     nodes_df = import_nodes_tntp(nodesfile)
 
     # add geometry coordinates to trips
-    # matrix_df = load_matrix_from_csv(flowscsvfile)
     matrix_df = _sample_n_instances(load_matrix_from_csv(flowscsvfile), 5000)
     trips = add_geometry(matrix_df, nodes_df)
 
@@ -171,16 +150,21 @@ def _sample_n_instances(df: pd.DataFrame, n: int) -> pd.DataFrame:
     """Randomly selects N instances from the DataFrame."""
     return df.sample(n=n, replace=False).reset_index(drop=True)
 
-def generate_trip_counts(df: DataFrame, n: int = 24) -> pd.DataFrame:
-    """Generate a DataFrame with n random trip counts using Poisson distribution based on OD flow."""
-    df[f'{n}h_trip_counts'] = df['od_flow'].apply(lambda x: np.sum(np.random.poisson(x, n)))
-    return df
+def generate_trip_counts(od_flow: float, n: int = 24) -> int:
+    """Generates trip count in N-hours interval using Poisson distribution.
+
+    :param n: Number of hours to generate trips for.
+    :param od_flow: OD flow (hourly occurrence of vehicle) between two nodes.
+    """
+    rng = np.random.default_rng()
+    trip_count = np.sum(rng.poisson(od_flow, n))
+    return trip_count
+
+def convert_TXT(city: str):
+    pass
 
 if __name__ == '__main__':
-    # convert_TNTP_format()
-    # generate_trips()
-    tripsfile = RESOURCE_PATH / "Sydney_trips.csv"
-    df = load_matrix_from_csv(tripsfile)
-    trip_counts = generate_trip_counts(df)
-    trip_counts.to_csv(tripsfile, index=False)
-    # pass
+    # convert_CSV_format("Porto")
+    # convert_TNTP_format("Sydney")
+    # generate_trips("Sydney")
+    # convert_TXT("Beijing")
--- python/darpinstances/instance_generation/demand_to_database.py
+++ python/darpinstances/instance_generation/demand_to_database.py
@@ -1,54 +1,39 @@
-import ast
-import csv
 import geopandas as gpd
 from geopandas import GeoDataFrame
-from shapely.geometry import Point, LineString
-from pathlib import Path
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 
 from darpinstances.credentials_config import CREDENTIALS as config
 from darpinstances.instance_generation.convert_formats import *
 
-def save_format(trips: GeoDataFrame, vehicles: GeoDataFrame):
-    """Save the trips and vehicles GeoDataFrames to a file."""
+def save_format(trips: GeoDataFrame):
+    """Save the trips to a SHP file."""
     trips['orig_wkt'] = trips['origin'].apply(lambda x: x.wkt)
     trips['dest_wkt'] = trips['destination'].apply(lambda x: x.wkt)
     trips.drop(columns=['origin', 'destination'], inplace=True)
 
     trips.to_file('resources/trips.shp', driver='ESRI Shapefile')
-    vehicles.to_file('resources/vehicles.shp', driver='ESRI Shapefile')
 
     print("Data saved to files.")
 
-def load_file() -> tuple[GeoDataFrame, GeoDataFrame]:
+def load_file() -> GeoDataFrame:
     """Load the trips and vehicles GeoDataFrames from a file."""
     trips = gpd.read_file('resources/trips.shp')
     trips['origin'] = gpd.GeoSeries.from_wkt(trips['orig_wkt'])
     trips['destination'] = gpd.GeoSeries.from_wkt(trips['dest_wkt'])
     trips.drop(columns=['orig_wkt', 'dest_wkt'], inplace=True)
 
-    vehicles = gpd.read_file('resources/vehicles.shp')
-    vehicles = vehicles.rename(columns={
-        'geometry': 'starting_position',
-    })
+    return trips
 
-    return trips, vehicles
-
-def convert_TXT_format():
-    pass
-
-def load_demand_to_db(trips: GeoDataFrame, vehicles: GeoDataFrame, schema: str):
+def load_demand_to_db(trips: GeoDataFrame | DataFrame, schema: str):
     port = config.db_server_port
     connection_uri = f"postgresql://{config.username}@{config.db_host}:{port}/{config.db_name}"
     engine = create_engine(connection_uri)
 
     trips.set_geometry("geometry", inplace=True)
-    vehicles.set_geometry("starting_position", inplace=True)
 
     try:
         trips.to_postgis("trips", engine, if_exists="append", index=False, schema=schema)
-        vehicles.to_postgis("vehicles", engine, if_exists="append", index=False, schema=schema)
 
         print("Data loaded into the database successfully.")
     except SQLAlchemyError as e:
