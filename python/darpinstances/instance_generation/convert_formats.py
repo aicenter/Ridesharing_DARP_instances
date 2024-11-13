cf55499e91a0a634727ffb03292bb37dd62ac567
Domi Sidlova <domisidlova@gmail.com>
2024-11-13 10:48:22 +0100

    enhance trajectory processing and file conversion for Beijing GPS trajectories

--- python/darpinstances/instance_generation/convert_formats.py
+++ python/darpinstances/instance_generation/convert_formats.py
@@ -7,14 +7,21 @@ from pandas import DataFrame
 from shapely.geometry import Point, LineString
 from datetime import datetime, timedelta
 from geopy.distance import geodesic
+from tqdm import tqdm
 
 from darpinstances.instance_generation.demand_generation import generate_uniform_trip_times
 
 PATH = Path(__file__).parent.parent.parent.parent
 RESOURCE_PATH = PATH / "resources/"
+
+# Beijing
+LONGITUDE_RANGE = (115, 118)
+LATITUDE_RANGE = (39, 42)
+
 BASETIME = datetime(2014, 1, 1)
 STOP_TIME_THRESHOLD = timedelta(minutes=5)
 DISTANCE_THRESHOLD = 0.05
+TRAJECTORY_THRESHOLD = timedelta(minutes=30)
 
 def import_nodes_tntp(file: str) -> DataFrame:
     """Loads nodes from a TNTP file and converts coordinates to Point geometry."""
@@ -156,111 +163,128 @@ def generate_trip_counts(od_flow: float, n: int = 24) -> int:
     trip_count = np.sum(rng.poisson(od_flow, n))
     return trip_count
 
-# def convert_TXT(city: str) -> DataFrame:
-#     input_folder = RESOURCE_PATH / f"{city}_trips_og"
-#     files = input_folder.glob("*.txt")
-#     trips = []
-#     for f in files:
-#         points = []
-#         timestamps = []
-#         with open(f, 'r') as file:
-#             lines = file.readlines()
-#             if not lines: continue
-
-#             for line in lines:
-#                 _, timestamp, longitude, latitude = line.strip().split(',')
-#                 point = Point(float(latitude), float(longitude))
-#                 points.append(point)
-#                 timestamps.append(timestamp)
-        
-#         if len(points) < 2:
-#             continue
-#         line = LineString(points)
-#         if not line.is_valid:
-#             continue
-        
-#         trip = {
-#             "timestamp": timestamps[0],
-#             "geometry": line,
-#         }
-#         trips.append(trip)
-#     df = pd.DataFrame(trips)
-#     return df
-
 def calculate_distance(p1: Point, p2: Point) -> float:
     """Calculates the distance between two points.
     
     :param p1: First point in form of (lon, lat).
-    :param p2: Second point in form of (lon, lat)."""
+    :param p2: Second point in form of (lon, lat).
+    :return: Distance between two points in kilometers."""
+    # convert to lat, lon format
     pt1 = (p1.y, p1.x)
     pt2 = (p2.y, p2.x)
-    return geodesic(pt1, pt2).meters
+    try:
+        return geodesic(pt1, pt2).km
+    except ValueError as e:
+        raise ValueError(f"Error calculating distance between {pt1} and {pt2}.") from e
 
 def convert_str_to_timestamp(time: str) -> datetime:
     return datetime.fromisoformat(time)
 
-def generate_trips_from_trajectories(traj: DataFrame) -> list[dict]:
+def is_within_coords_range(lat: float, lon: float) -> bool:
+    """Checks if coordinates are within specified bounds."""
+    return (LATITUDE_RANGE[0] <= lat <= LATITUDE_RANGE[1] and 
+            LONGITUDE_RANGE[0] <= lon <= LONGITUDE_RANGE[1])
+
+def process_record(record: str) -> dict | None:
+    """Converts record into dictionary with timestamp and Point geometry."""
+    _, time, lon, lat = record.strip().split(',')
+    if not is_within_coords_range(float(lat), float(lon)):
+        return None
+    timestamp = convert_str_to_timestamp(time)
+    point = Point(float(lon), float(lat))
+    return {"timestamp": timestamp, "geometry": point}
+
+def process_trajectory(trajectory: list[str]) -> list[dict]:
+    """Process a trajectory and extract trips from it 
+    based on: https://arxiv.org/pdf/1602.00994."""
     trips = []
     i = 0
-    while i < len(traj) - 1:
-        start_point = traj.iloc[i]
+    while i < len(trajectory) - 1:
+        start_point = process_record(trajectory[i])
+        if start_point is None:  # skip invalid points
+            i += 1
+            continue
+        current_point = start_point
         j = i + 1
-        trip_started = False
-        while j < len(traj):
-            end_point = traj.iloc[j]
-            
-            # skip identical entries
-            if start_point['timestamp'] == end_point['timestamp']:
+        trip_formed = False
+        # find destination
+        while j < len(trajectory):
+            next_point = process_record(trajectory[j])
+            if next_point is None:  # skip invalid points
                 j += 1
                 continue
 
-            # is taxi leaving?
-            if calculate_distance(start_point['geometry'], end_point['geometry']) > DISTANCE_THRESHOLD:
-                # If the stop lasted long enough, mark it as a trip start
-                if (convert_str_to_timestamp(end_point['timestamp']) - convert_str_to_timestamp(start_point['timestamp'])) >= STOP_TIME_THRESHOLD:
+            distance = calculate_distance(current_point['geometry'], next_point['geometry'])
+            
+            if distance <= DISTANCE_THRESHOLD:
+                time_diff = next_point['timestamp'] - current_point['timestamp']
+                if time_diff >= STOP_TIME_THRESHOLD:
                     trips.append({
                         'origin': start_point['geometry'],
-                        'destination': end_point['geometry'],
+                        'destination': next_point['geometry'],
                         'timestamp': start_point['timestamp']
                     })
-                    trip_started = True
-                break
+                    i = j
+                    trip_formed = True
+                    break
+                current_point = next_point
+            else:
+                current_point = next_point
             j += 1
-        
-        if trip_started:
-            i = j
-        else:
+        if trip_formed and current_point != start_point:
+            trips[-1]['destination'] = current_point['geometry']
+        if not trip_formed:
             i += 1
-    
     return trips
-
-def process_trajectory(traj: list[str]) -> list[dict]:
-    points = []
-    for record in traj:
-        _, timestamp, longitude, latitude = record.strip().split(',')
-        timestamp_dt = datetime.fromisoformat(timestamp)
-        if timestamp_dt.date() != datetime.fromisoformat("2008-02-02").date():
-            continue
-        point = Point(float(longitude), float(latitude))
-        trip = {
-            "timestamp": timestamp,
-            "geometry": point,
-            }
-        points.append(trip)
-    return points
 
 def convert_TXT(city: str) -> DataFrame:
     input_folder = RESOURCE_PATH / f"{city}_trips_og"
-    files = input_folder.glob("*.txt")
+    output_folder = RESOURCE_PATH / f"{city}_trips"
+    files = list(input_folder.glob("*.txt"))
+    total_files = len(files)
+    long_files = show_files(city)
+    with tqdm(total=total_files, desc="Processing files", unit="file") as pbar:
+        for f in files:
+            if f.stem in long_files:
+                continue
+            output_file = RESOURCE_PATH / f"{f.stem}.csv"
+            if output_file.exists():
+                pbar.update(1)
+                continue
+            print("FILE: ", f)
+            with open(f, 'r') as file:
+                lines = file.readlines()
+                if not lines: 
+                    pbar.update(1)
+                    continue
+                trajectory = process_trajectory(lines)
+            output_file = output_folder / f"{f.stem}.csv"
+            pd.DataFrame(trajectory).to_csv(output_file, index=False)
+            pbar.update(1)
+    return pd.DataFrame(trajectory) if trajectory else pd.DataFrame()
+
+def show_files(city: str) -> list:
+    input_folder = RESOURCE_PATH / f"{city}_trips_og"
+    files = list(input_folder.glob("*.txt"))
+    long_files = []
     for f in files:
         with open(f, 'r') as file:
             lines = file.readlines()
-            if not lines: continue
-            traj = process_trajectory(lines)
-        break
-    df = pd.DataFrame(traj)
-    trips = generate_trips_from_trajectories(df)
-    return pd.DataFrame(trips)
+            lines_count = len(lines)
+            if lines_count > 20000:
+                long_files.append(f.stem)
+    return long_files
+
+def count_trips(city: str) -> int:
+    input_folder = RESOURCE_PATH / f"{city}_trips"
+    files = list(input_folder.glob("*.csv"))
+    s = 0
+    for f in files:
+        with open(f, 'r') as file:
+            lines = file.readlines()
+            lines_count = len(lines) - 1
+            s += lines_count
+    return s
 
 def generate_and_save_csv(city: str):
     # save trips with geometry
@@ -280,4 +304,6 @@ if __name__ == '__main__':
     # generate_and_save_csv("Sydney")
     # convert_TXT("Beijing")
     generate_and_save_csv("Beijing")
-    # pass
+    # print(show_files("Beijing"))
+    # print(count_trips("Beijing"))
+
